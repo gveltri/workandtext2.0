0950189f4c667f8240cdcb9951fcc4229db745a0
Gerardo Veltri <gerardo.veltri@gmail.com>
2015-07-17 18:04:33 -0400

    just playing with two.ks

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,15 @@
+// Main Javascript script for workandtext.com
+// 
+// Gerardo Veltri
+// 2015
+// 
+
+
+// var elem = document.getElementById('animation');
+// var two = new Two().appendTo(elem);
+
+// var dot = two.makeCircle(120, 120, 100);
+// dot.fill = '#52C5DC';
+// dot.noStroke();
+
+// two.update();            
